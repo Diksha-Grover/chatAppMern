2d794a9ed02f21cbb54aabd490d3a1b910776002
dikshaaGrover <diksha.grover@mtap.in>
2024-05-07 12:59:17 +0530

    reverted the url for host

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatappmern-lxvg.onrender.com";
+export const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
