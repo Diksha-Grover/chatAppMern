50c0dc854ab858c87a3cb1e48e78fd5200dcb03c
dikshaaGrover <diksha.grover@mtap.in>
2024-05-08 12:58:42 +0530

    checking

--- server/routes/messagesRoute.js
+++ server/routes/messagesRoute.js
@@ -1,7 +1,6 @@
 const { addMessage, getAllMessage } = require("../controllers/messagesController");
   
   const router = require("express").Router();
-  // checking
   
   router.post("/addmsg/", addMessage);
   router.post("/getmsg/", getAllMessage);
