ee2cd0f3de067a87b9c6f02d935ef867753e11df
dikshaaGrover <diksha.grover@mtap.in>
2024-05-14 11:59:39 +0530

    vercel link

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,9 +1,11 @@
-export const host = "mongodb+srv://chat-app:1234567890@chatappcluster.86kudfl.mongodb.net/?retryWrites=true&w=majority&appName=chatAppCluster";
+export const host = "https://chat-app-mern-backend-five.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
 export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
+
+// mongodb+srv://chat-app:1234567890@chatappcluster.86kudfl.mongodb.net/?retryWrites=true&w=majority&appName=chatAppCluster
 
 
